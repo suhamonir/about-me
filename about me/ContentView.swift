ab9ab41f6677f620ea6babdf9b08fde9895ba07d
Josephine Serrano <josephineserrano07@gmail.com>
2023-07-27 13:14:57 -0400

    changed background color to brown

--- about me/ContentView.swift
+++ about me/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
    
     var body: some View {
         ZStack{
-            Color(.systemGray2)
+            Color(.systemBrown)
                 .ignoresSafeArea()
             
             VStack {
